39673b77227b2bf6d160d8d722c5bb5217e94756
Avinash Shenoy <avi123shenoy@hotmail.com>
2017-11-06 20:36:37 +0530

    Preprocessed

--- init.py
+++ init.py
@@ -1,22 +1,34 @@
-import pandas, sklearn, numpy
+import pandas, sklearn, numpy as np
 from datetime import datetime as dt
 from dateutil.parser import parse
 
 def init():
     # Load input data
     ipdata = pandas.read_csv("data/bitcoin_dataset.csv")
+
+    # Drop rows with NaN
+    for key in ipdata:
+        try:
+            ipdata = ipdata[np.isfinite(ipdata[key])]
+        except:
+            pass
+
     #ipdata['diff'] = pandas.Series([0] * len(ipdata['btc_market_price']), index = ipdata.index)
     #ipdata['dir'] = pandas.Series([0] * len(ipdata['btc_market_price']), index = ipdata.index)
     ipdata['next'] = pandas.Series([0] * len(ipdata['btc_market_price']), index = ipdata.index)
 
     # Convert Date field to Python Date type
     #ipdata['date'] = pandas.Series([dt(1,1,1,1,1,1)] * len(ipdata['btc_market_price']), index = ipdata.index)
-    for ind in range(len(ipdata['Date'])):
+    for ind in ipdata.index:
         ipdata.ix[ind, 'Date'] = parse(ipdata['Date'][ind])
 
     # Add next day's price
-    for ind in range(len(ipdata['btc_market_price']) - 1):
-        ipdata.ix[ind, 'next'] = ipdata['btc_market_price'][ind+1]
+    for ind in ipdata.index:
+        try:
+            ipdata.ix[ind, 'next'] = ipdata['btc_market_price'][ind+1]
+        except:
+            if ind == max(ipdata.index):
+                pass
         '''
         # Legacy code
         # Get difference in market price between every row and the next
@@ -30,4 +42,4 @@ def init():
             ipdata.ix[ind, 'dir'] = True
         '''
 
-    return ipdata.drop([len(ipdata['Date']) - 1])
+    return ipdata.drop([max(ipdata.index)])
